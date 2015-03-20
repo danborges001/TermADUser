27ac9c2a1341d23c930a1f3242836de183ce6cc6
danborges001 <danborges78@gmail.com>
2015-03-20 15:58:14 -0700

    Added commented out lines.
    Added older version commented out for historical reference, fixed a
    line.

--- TermADUser.ps1
+++ TermADUser.ps1
@@ -6,12 +6,27 @@ Get-ADUser -filter {EmailAddress -like $Email}
 #Set variables, perform terminate procedures on user account
 $Username = Get-ADUser -filter {EmailAddress -like $Email}
 $TermDate = Get-Date -UFormat "%Y-%m-%d"
-Move-ADObject -TargetPath "OU=TermedUsers,OU=Actian Corporation,DC=Actian,DC=com"
+Move-ADObject  $Username -TargetPath "OU=TermedUsers,OU=Actian Corporation,DC=Actian,DC=com"
 Set-ADUser $Username.SAMAccountName -Clear Department,Title,telephoneNumber,Company,Manager,Mobile,HomePhone
-Set-ADUser $Username.SamAccountName -Replace @{Description="Termed "+ $TermDate}
-Set-ADUser $Username.SamAccountName -Enabled $False
+Set-ADUser $Username.SAMAccountName -Replace @{Description="Termed "+ $TermDate}
+Set-ADUser $Username.SAMAccountName -Enabled $False
 $GroupArray = Get-ADGroup -property * -filter * | Where-Object {$_.member -like $Username}
 ForEach ($Group in $GroupArray)
 	{
 	Remove-ADGroupMember -Identity $NewGroup -Member $Username.SAMaccountName -Confirm:$false
-	}          
+	}
+#param(
+#[string]$Email
+#)
+#Get-ADUser -filter {EmailAddress -like $Email} | Move-ADObject -TargetPath "OU=TermedUsers,OU=Actian Corporation,DC=Actian,DC=com"
+#Get-ADUser -filter {EmailAddress -like $Email}
+#$Username = Get-ADUser -filter {EmailAddress -like $Email} | Select-Object SAMAccountName
+#Set-ADUser $Username.SAMAccountName -Clear Department,Title,telephoneNumber,Company,Manager,Mobile,HomePhone
+#$TermDate = Get-date -UFormat "%Y-%m-%d"
+#Set-ADUser $Username.SamAccountName -Replace @{Description="Termed "+ $TermDate}
+#Set-ADUser $Username.SamAccountName -Enabled $False
+#$GroupArray = Get-ADPrincipalGroupMembership $UserName.SAMAccountName | Select name | Where-Object {$_.name -ne "Domain Users"}
+#ForEach ($Group in $GroupArray)
+#	{
+#	Remove-ADGroupMember -Identity $Group.Name -Member $Username.SAMAccountName -Confirm:$false
+#	}
