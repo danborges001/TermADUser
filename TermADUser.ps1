ff1981a9509815384042c4c82f24d979575c936d
danborges001 <danborges78@gmail.com>
2015-06-03 09:19:39 -0700

    Updated functionality
    Added line that hides users from the global address list.
    Fixed typo.

--- TermADUser.ps1
+++ TermADUser.ps1
@@ -10,23 +10,9 @@ Move-ADObject  $Username -TargetPath "OU=TermedUsers,OU=Actian Corporation,DC=Ac
 Set-ADUser $Username.SAMAccountName -Clear Department,Title,telephoneNumber,Company,Manager,Mobile,HomePhone
 Set-ADUser $Username.SAMAccountName -Replace @{Description="Termed "+ $TermDate}
 Set-ADUser $Username.SAMAccountName -Enabled $False
+Set-ADObject $Username -Replace @{msExchHideFromAddressLists=$True}
 $GroupArray = Get-ADGroup -property * -filter * | Where-Object {$_.member -like $Username}
 ForEach ($Group in $GroupArray)
 	{
-	Remove-ADGroupMember -Identity $NewGroup -Member $Username.SAMaccountName -Confirm:$false
+	Remove-ADGroupMember -Identity $Group -Member $Username.SAMaccountName -Confirm:$false
 	}
-#param(
-#[string]$Email
-#)
-#Get-ADUser -filter {EmailAddress -like $Email} | Move-ADObject -TargetPath "OU=TermedUsers,OU=Actian Corporation,DC=Actian,DC=com"
-#Get-ADUser -filter {EmailAddress -like $Email}
-#$Username = Get-ADUser -filter {EmailAddress -like $Email} | Select-Object SAMAccountName
-#Set-ADUser $Username.SAMAccountName -Clear Department,Title,telephoneNumber,Company,Manager,Mobile,HomePhone
-#$TermDate = Get-date -UFormat "%Y-%m-%d"
-#Set-ADUser $Username.SamAccountName -Replace @{Description="Termed "+ $TermDate}
-#Set-ADUser $Username.SamAccountName -Enabled $False
-#$GroupArray = Get-ADPrincipalGroupMembership $UserName.SAMAccountName | Select name | Where-Object {$_.name -ne "Domain Users"}
-#ForEach ($Group in $GroupArray)
-#	{
-#	Remove-ADGroupMember -Identity $Group.Name -Member $Username.SAMAccountName -Confirm:$false
-#	}
